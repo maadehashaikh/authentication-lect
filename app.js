2abd79663a8a2fa199e1157256dc32f1289e1a4c
Maadeha shaikh <maadeham@gmail.com>
2024-08-04 22:03:43 +0500

    update

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 // npm i express bcrypt jsonwebtoken cookie-parser
 
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set("view engine" , "ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use()
+app.use(express.static(path.join(__dirname,'public')));
+app.use(cookieParser());
+
 
 app.get('/',function(req,res){
-  res.send("Welcome");
+  res.render("index");
 })
 
 app.listen(3000);
